22fde2fa7786cb2b12c74590018e3a630c497052
Faragau Giulia <giuliamenisa49@gmail.com>
2023-01-05 21:28:30 +0200

    add hideAllPages

--- index.js
+++ index.js
@@ -4,10 +4,37 @@ var skills = ["HTML", "CSS", "JS", "Drive"];
 
 var skillsHTML = "";
 
-for (var i = 0; i < skills.length; ) {
+for (var i = 0; i < skills.length; i++) {
   skillsHTML += "<li>" + skills[i] + "</li>";
 }
 
 skillsEl.innerHTML = skillsHTML;
 
-document.getElementById("home").style.dislpay = "block";
+function hideAllPages() {
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("langueges").style.display = "none";
+  document.getElementById("project").style.display = "none";
+  document.getElementById("home").style.display = "none";
+}
+
+function dislpayHome() {
+  hideAllPages();
+  document.getElementById("home").style.display = "block";
+}
+function displaySkills() {
+  hideAllPages();
+  console.warn("trebuie sa afisam skills page");
+
+  document.getElementById("skills").style.display = "block";
+}
+function displayLanguages() {
+  hideAllPages();
+  document.getElementById("languages").style.display = "block";
+}
+function displayProject() {
+  hideAllPages();
+  document.getElementById("project").style.display = "block";
+}
+
+dislpayHome();
